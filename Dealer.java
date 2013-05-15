93ba34e86d6e3d975c1b78065425d1ac6b79fdde
Alex Yang <alex.lingyun.yang@gmail.com>
2013-05-15 17:45:36 -0400

    Dealer Class

--- Dealer.java
+++ Dealer.java
@@ -5,4 +5,12 @@
  * Java 1.7, MacOSX10.8
  * March 14-21, 2013
  */
+import java.util.*;
+public class Dealer extends Player {
+	public void draw(ArrayList<Card> deck) {
+		if (getValue() < 17){
+			super.draw(deck);
+		}
+	}
+}
 
