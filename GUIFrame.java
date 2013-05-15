3049d6f0a1296b4c9f27671585f3d921408f932f
Alex Yang <alex.lingyun.yang@gmail.com>
2013-05-15 18:23:25 -0400

    GUIFrame update

--- GUIFrame.java
+++ GUIFrame.java
@@ -1,4 +1,23 @@
-
-public class GUIFrame {
-
+/**
+ * Sophia Anopa/Alex Yang
+ * APCSSec01Yl12
+ * Blackjack: Main/GUI
+ * Java 1.7, MacOSX10.8
+ * May 14-21, 2013
+ */
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class GUIFrame extends JFrame{
+	JPanel pane = new JPanel();
+	GUIFrame() {
+		super("Black Jack"); setBounds(100, 100, 300, 100);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container con = this.getContentPane();
+		con.add(pane);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		new GUIFrame();
+	}
 }
