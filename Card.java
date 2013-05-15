ff2a52b276be38be36d7a65afe46b24fd5816626
Sophia Anopa <sanopa@gmail.com>
2013-05-14 23:34:15 -0400

    more details to Card - before trying out enums

--- Card.java
+++ Card.java
@@ -8,6 +8,8 @@ public class Card
 {
     private int suit; //1 = Clubs, 2 = Diamonds, 3 = Hearts, 4 = Spades
     private String value;
+    private int numvalue;
+    public static int[] numvals = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
     
     /**
      * Default Constructor
@@ -18,9 +20,17 @@ public class Card
     /**
      * Normal Constructor
      */
-    public Card(int s, String v) {
+    public Card(int s, String v, int n) {
         suit = s;
         value = v;
+        numvalue = n;
+    }
+    
+    /**
+     * Mutators
+     */
+    public void setNumValue(int n) {
+        numvalue = n;
     }
     
     /**
@@ -32,6 +42,10 @@ public class Card
     
     public String getValue() {
         return value;
+    }
+    
+    public int getNumValue() {
+        return numvalue;
     }
     
     /**
